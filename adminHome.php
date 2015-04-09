81e5b06d59f99301d9c8869de2a608ee71eb55d5
Deepanshu Gautam <deepanshu09.subhash@gmail.com>
2015-04-09 19:30:29 +0530

    updated admin Home

--- adminHome.php
+++ adminHome.php
@@ -64,6 +64,8 @@
 					echo mysqli_error();
 				}
 				unset($_SESSION['dbName']);
+				unset($_SESSION['year']);
+				unset($_SESSION['semester']);
 			}
 			if(isset($_POST['activate_database']))
 			{
@@ -79,56 +81,12 @@
 					{
 						echo mysqli_error();
 					}
-				}
-				
-			}
-			if(isset($_POST['select_database']))
-			{			
-			}
-			if(isset($_POST['submit']))
-			{
-				echo "<script>$('#select').modal('show');</script>";
-			}
+				}				
+			}					
 		?>
 		
-		<form action="adminHome.php" method="post">				
-			<div class="modal fade" id="activate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h4 class="modal-title" id="myModalLabel">Activate Database</h4>
-						</div>
-						<div class="modal-body">
-							Activate the client?
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-							<button type="submit" onclick="activate_database()" name="activate_database" class="btn btn-primary">Yes</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		
-			
-			<div class="modal fade" id="deactivate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h4 class="modal-title" id="myModalLabel">Deactivate Database</h4>
-						</div>
-						<div class="modal-body">
-							De-Activate the client?
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-							<button type="submit" onclick="deactivate_database()" name="deactivate_database"class="btn btn-primary">Yes</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		
+		<form action="adminHome.php" method="post">																			
+
 			<div class="modal fade" id="select_database" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
@@ -147,6 +105,11 @@
 				</div>
 			</div>		
 
+			<?php
+				if(!isset($_POST['select']))
+				{
+
+				echo '
 			<div class="modal fade" id="selected_database" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
@@ -155,7 +118,7 @@
 							<h4 class="modal-title" id="myModalLabel">Alert!</h4>
 						</div>
 						<div class="modal-body">
-							<?php									
+			';						
 								$query = "select dbName,year,semester from db_list where activeStatus = 1";						
 								$queryResult = mysqli_query($masterDbConnection,$query);
 								if($queryResult)
@@ -163,7 +126,7 @@
 									$queryRows = mysqli_num_rows($queryResult);
 									if($queryRows == 0)
 									{
-									  	echo '<script> $("#select_database").modal("show");</script>';					
+									  	echo "<script> $('#select_database').modal('show');</script>";					
 									}
 									else
 									{
@@ -179,6 +142,8 @@
 										{
 											$sem = "Odd";
 										}
+										$_SESSION['year'] = $array['year'];
+										$_SESSION['semester'] = $sem;
 										$msg = "Selected database is of ".$sem." semester of year ".$array["year"];
 										echo "<script type='text/javascript'>$('#selected_database').modal('show')</script>";
 									}
@@ -188,8 +153,8 @@
 									echo mysqli_error();
 								}
 								echo $msg;																											
-						?>
-
+						
+					echo '
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Ok</button>
@@ -197,8 +162,21 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>';
+			}
+			?>
+		</form>
 
+		<?php
+			if(isset($_POST['select']))
+			{										
+				$year = $_POST['year'];	
+				$semester = $_POST['semester'];
+
+			} 
+		?>
+
+		<form action="adminHome.php" method="post" id="modal_form">
 			<div class="modal fade" id="select" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
@@ -216,7 +194,7 @@
 									<input id="year" name="year" type="text" class="form-control" placeholder="Year"
 										value=
 											<?php
-												if(isset($_POST['submit']))
+												if(isset($_POST['select']))
 												{										
 													echo $year;	
 												} 
@@ -228,8 +206,8 @@
 										<label class="col-md-3">Semester*</label>												
 									</div>
 									<select id='semester' name='semester' class='discipline_options form-control'>
-										<option value=1 <?php if(isset($_POST['submit'])) if($semester == 1) echo "selected";?> >Even(E)</option>
-										<option value=0 <?php if(isset($_POST['submit'])) if($semester == 0) echo "selected";?> >Odd(O)</option>											
+										<option value=1 <?php if(isset($_POST['select'])) if($semester == 1) echo "selected";?> >Even(E)</option>
+										<option value=0 <?php if(isset($_POST['select'])) if($semester == 0) echo "selected";?> >Odd(O)</option>											
 									</select>
 								</div>
 							</div>
@@ -243,6 +221,108 @@
 			</div>
 
 		</form>	
+										
+
+		<?php
+			if(isset($_POST['select']))
+			{	
+				$querySB = "select dbName,year,semester from db_list where year='".$year."' and semester='".$semester."'";
+				$queryResultSB = mysqli_query($masterDbConnection,$querySB);
+				if($queryResultSB)
+				{
+					$queryRowsSB = mysqli_num_rows($queryResultSB);
+					if($queryRowsSB == 0)
+					{						
+						echo "<script>	
+							alert('No database existing!');	
+							$('#select').modal('show');
+						</script>";
+					}
+					else
+					{
+						$array = mysqli_fetch_array($queryResultSB);
+
+						$_SESSION['dbName'] = $array['dbName'];
+						if($array['semester'] == 1)
+						{
+							$sem = "Even";
+						}
+						else
+						{
+							$sem = "Odd";
+						}
+						$_SESSION['year'] = $array['year'];
+						$_SESSION['semester'] = $sem;
+
+						$msg = "Selected database is of ".$sem." semester of year ".$array["year"];
+						echo '
+						<div class="modal fade" id="chosen_database" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+							<div class="modal-dialog">
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+										<h4 class="modal-title" id="myModalLabel">Alert!</h4>
+									</div>
+									<div class="modal-body">					
+						';			echo $msg;
+						echo '
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+										<button data-toggle="modal" data-target="#select" type="button" data-dismiss="modal" name="select_database" class="btn btn-primary">Select</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						';
+						echo "<script type='text/javascript'>$('#chosen_database').modal('show')</script>";
+					}
+				}
+				else
+				{
+					echo mysqli_error();
+				}
+			}
+		?>
+
+		<form action="adminHome.php" method="post">
+			<div class="modal fade" id="activate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<h4 class="modal-title" id="myModalLabel">Activate Database</h4>
+						</div>
+						<div class="modal-body">
+							Activate the client?
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+							<button type="submit" onclick="activate_database()" name="activate_database" class="btn btn-primary">Yes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="modal fade" id="deactivate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<h4 class="modal-title" id="myModalLabel">Deactivate Database</h4>
+						</div>
+						<div class="modal-body">
+							<?php echo "Deactivate the database of ".$_SESSION['semester']." of year ".$_SESSION['year']."?"?>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+							<button type="submit" onclick="deactivate_database()" name="deactivate_database"class="btn btn-primary">Yes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+
 	</body>
 </html>
 
@@ -253,8 +333,18 @@
 	function activate_database(){	   	    
 	    $('#activate').modal('hide');
 	}	
-	function select_check(){
-		alert("hi");
-		//$('#select').modal('show');
-	}
+	$("#modal_form").submit(function(e) {
+		//alert("hi");
+		
+		var year = document.getElementById('year').value;
+		//var semester = document.getElementById('semester').value;
+
+		//alert(year);
+		//alert(year.length);
+		if(year.length == 0)
+		{
+			alert("Fill in the year");
+			e.preventDefault();
+		}
+	});
 </script>
